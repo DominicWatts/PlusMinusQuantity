f4a566e7769f4525a6b14565b95ea3f84c8039d5
Dominic <dominic.watts@hotmail.co.uk>
2019-07-29 21:35:18 +0100

    base

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,12 @@
+/**
+ *
+ */
+var config = {
+    map: {
+        '*': {
+            'Magento_Checkout/template/minicart/item/default.html': 'Xigen_PlusMinusQuantity/template/minicart/item/default.html',
+            'sidebar': 'Xigen_PlusMinusQuantity/js/sidebar',
+            'Magento_Checkout/js/view/minicart': 'Xigen_PlusMinusQuantity/js/view/minicart'
+        }
+    }
+};
